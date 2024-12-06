29126f859e22e68cb115300ad82fbf666b060c79
adsRole-garg-anuj <159473985+adsRole-garg-anuj@users.noreply.github.com>
2024-12-07 03:35:48 +0530

    push new things

--- src/App.js
+++ src/App.js
@@ -3,9 +3,23 @@ import "./App.css";
 import CareerFormPopUp from "./components/CareerFormPopUp";
 
 function App() {
+  const [isCareerFormOpen, setIsCareerFormOpen] = useState(false);
+  const [careerModal, setCareerModal] = useState("TRU Caller");
+
+  const handleJobForm = useCallback((selectedJobTitle) => {
+    setIsCareerFormOpen((prevProps) => !prevProps);
+    setCareerModal(selectedJobTitle);
+  }, []);
   return (
     <div className="">
-      <CareerFormPopUp />
+      {/* <CareerFormPopUp /> */}
+      {isCareerFormOpen && (
+        <CareerFormPopUp
+          isOpen={isCareerFormOpen}
+          onClose={handleJobForm}
+          formName={careerModal}
+        />
+      )}
     </div>
   );
 }
